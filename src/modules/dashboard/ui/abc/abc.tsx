f160274f4231cef97f6dc60ccec4c46a0b5c5400
Lakshay Bansal <lakshay012003@gmail.com>
2025-07-28 21:20:56 +0530

    Fixed ui

--- src/modules/dashboard/ui/abc/abc.tsx
+++ src/modules/dashboard/ui/abc/abc.tsx
@@ -1,10 +1,15 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Experiment = () => {
   return (
     <div>
       Hello world from the experiment component!
+      <Button>
+        Click me for an experiment
+      </Button>
     </div>
+    
   )
 }
 
