08d20bea11a4282e0b4a1f24c36162e5b8a38259
Lakshay Bansal <lakshay012003@gmail.com>
2025-08-18 23:10:45 +0530

    Trpc setup 3

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,8 +2,10 @@ import { auth } from "@/lib/auth";
 import { HomeView } from "@/modules/home/ui/views/home-view"
 import { redirect } from "next/navigation";
 import { headers } from "next/headers";
+import { caller } from "@/trpc/server";
 
 const Page = async () => {
+  const greeting = await caller.hello({ text: "Lakshay from server" });
   const session = await auth.api.getSession({
     headers: await headers(),
   });
@@ -12,7 +14,7 @@ const Page = async () => {
     redirect("/sign-in");
   }
 
-  return <HomeView/>
+  return <HomeView greeting = {greeting}/> // You can pass the greeting to HomeView if needed, e.g., <HomeView greeting={greeting} /> 
 }
 
 export default Page;
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,30 +1,16 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
 
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
- export const HomeView = () => {
-    const router = useRouter();
-
-  const {data: session} = authClient.useSession();
-
-  if(!session){
-    return( 
-      <p>Loading ...</p>
-    )
-  }
-
+ export const HomeView = ({greeting}) => {
+  const trpc = useTRPC();
+  //const {data} = useQuery(trpc.hello.queryOptions({text: "Lakshay !"}));
 
   return (
    <div className="flex flex-col p-4 gap-y-4">
-    <p>Logged in as {session.user.name}</p>
-    <Button className="w-full hover:bg-green-600 hover:cursor-pointer bg-green-700 text-white font-bold" onClick={() => authClient.signOut({
-        fetchOptions:{ 
-            onSuccess: () => router.push("/sign-in")},
-    })}>
-      Sign out
-    </Button>
+      {/* <p className="text-lg">{data?.greeting}</p> */}
+      <p className="text-lg">{greeting.greeting}</p>
    </div>
   );
 }
