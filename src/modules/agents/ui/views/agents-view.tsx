cb39734c6f9d725ab5073f345bcc0c82b9945cbb
Lakshay Bansal <lakshay012003@gmail.com>
2025-08-22 23:50:29 +0530

    Fixed ui

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -4,6 +4,8 @@ import {useSuspenseQuery} from "@tanstack/react-query";
 import {useTRPC} from "@/trpc/client";
 import { LoadingState } from "@/components/loading-states";
 import { ErrorState } from "@/components/error-states";
+import { ResponsiveDialog } from "@/components/responsive-dialog";
+import { Button } from "@/components/ui/button";
 
 export const AgentsView = () => {
   const trpc = useTRPC();
@@ -11,6 +13,14 @@ export const AgentsView = () => {
 
   return (
     <div className="flex flex-col p-4 gap-y-4">
+      <ResponsiveDialog
+      title = "Agent Details"
+      description = "More information about the agent"
+      open = {true}
+      onOpenChange = {() => {}}
+      >
+        <Button className="bg-amber-200">Open Dialog</Button>
+      </ResponsiveDialog>
         {JSON.stringify(data, null, 2)}
     </div>
   );
