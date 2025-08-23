b75e3047f2403f3d5abf9c9b8758ac2bf4b69b05
Lakshay Bansal <lakshay012003@gmail.com>
2025-08-23 22:59:55 +0530

    Dashboard command mobile view component updated and working fine

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -9,6 +9,8 @@ export const AgentsView = () => {
   const trpc = useTRPC();
   const {data} = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
+
+
   return (
     <div className="flex flex-col p-4 gap-y-4">
         {JSON.stringify(data, null, 2)}
