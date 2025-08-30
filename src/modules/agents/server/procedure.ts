b671b45bf8b58b19beb0a2dda574b4911fb019e0
Lakshay Bansal <lakshay012003@gmail.com>
2025-08-30 23:35:12 +0530

    Tried setting agents

--- src/modules/agents/server/procedure.ts
+++ src/modules/agents/server/procedure.ts
@@ -1,12 +1,25 @@
-import { createTRPCRouter,baseProcedure } from "@/trpc/init";
+import { createTRPCRouter,baseProcedure, protectedProcedure } from "@/trpc/init";
 import { agents } from "@/db/schema";
 import { db } from "@/db";
+import { agentsInsertSchema } from "../schemas";
 
 export const agentsRouter = createTRPCRouter({
     getMany: baseProcedure.query(async () => {
         const data = await db.select().from(agents);
-        await new Promise(resolve => setTimeout(resolve, 6000)); // Simulate a delay
-        //throw new Error("Simulated error for testing purposes");
+        
         return data;
-    })
+    }),
+    create: protectedProcedure
+        .input(agentsInsertSchema)
+        .mutation(async ({input,ctx}) => {
+            const [ createdAgent ] = await db
+            .insert(agents)
+            .values({
+                ...input,
+                userId : ctx.auth.user.id
+            })
+            .returning();
+
+            return createdAgent;
+        }),
 })
