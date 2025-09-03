0862523928257261240f0a3d8b12b633432582af
Lakshay Bansal <lakshay012003@gmail.com>
2025-09-03 23:46:26 +0530

    creating tables 2

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -4,16 +4,24 @@ import {useSuspenseQuery} from "@tanstack/react-query";
 import {useTRPC} from "@/trpc/client";
 import { LoadingState } from "@/components/loading-states";
 import { ErrorState } from "@/components/error-states";
+import { DataTable } from "../components/data-table";
+import { columns, Payment } from "../components/columns";
 
 export const AgentsView = () => {
   const trpc = useTRPC();
   const {data} = useSuspenseQuery(trpc.agents.getMany.queryOptions());
-  console.log(data);
-  console.log("Rendering AgentsView");
 
+  const mockData: Payment[] = [
+    { id: '1', amount: 100, status: 'success', email: 'abc@gmail.com'},
+    { id: '2', amount: 200, status: 'pending', email: 'bcd@gmail.com'},
+    { id: '3', amount: 300, status: 'failed', email: 'efg@gmail.com'},
+    { id: '4', amount: 400, status: 'processing', email: 'xyz@gmail.com'},
+    { id: '5', amount: 500, status: 'success', email: 'lakshay123@gmail.com'},
+  ]
+  
   return (
     <div className="flex flex-col p-4 gap-y-4">
-        {JSON.stringify(data, null, 2)}
+        <DataTable columns={columns} data={mockData}/>
     </div>
   );
 };
