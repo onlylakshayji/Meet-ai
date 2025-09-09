155b7a0fb72a0bf77a09a9ec355b2e2bd10e88e0
Lakshay Bansal <lakshay012003@gmail.com>
2025-09-10 00:00:58 +0530

    creating tables 4

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -18,10 +18,16 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
-    name: z.string().min(1, "Name is required"),
-    email: z.string().email("Invalid email address"),
+    name: z.string().trim().min(1, "Name is required").max(100,"Name is too long").regex(/^[a-zA-Z\s'-]+$/, "Invalid name"),
+    email: z.string().email("Invalid email address").transform((val) => val.toLowerCase()),
     password: z.string().min(1, "Password is required"),
-    confirmPassword: z.string().min(1, "Confirm Password is required")
+    confirmPassword: z.string()
+    .min(8, "Password must be at least 8 characters")
+    .max(64, "Password too long")
+    .regex(/[A-Z]/, "Must contain an uppercase letter")
+    .regex(/[a-z]/, "Must contain a lowercase letter")
+    .regex(/[0-9]/, "Must contain a number")
+    .regex(/[^A-Za-z0-9]/, "Must contain a special character"),
 }).refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match",  
     path: ["confirmPassword"],
