d01ea906bad6500ae64046249da8ce1e87ee31ed
Lakshay Bansal <lakshay012003@gmail.com>
2025-09-10 23:10:28 +0530

    creating types

--- src/modules/agents/types.ts
+++ src/modules/agents/types.ts
@@ -3,4 +3,8 @@ import { inferRouterOutputs } from "@trpc/server";
 import type {AppRouter} from "@/trpc/routers/_app";
 
 export type AgentGetOne = inferRouterOutputs<AppRouter>['agents']['getOne'];
+
+
 export type AgentGetMany = inferRouterOutputs<AppRouter>['agents']['getMany'][number];
+
+export type AgentCreate = inferRouterOutputs<AppRouter>['agents']['create']; // Added myself , its extra and not used yet
