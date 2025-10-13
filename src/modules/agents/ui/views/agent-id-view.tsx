cdd374d20a915d8ba3481600ae1fd83616b9a6c2
Lakshay Bansal <lakshay012003@gmail.com>
2025-10-13 22:09:09 +0530

    Implemented remove-agent on ui

--- src/modules/agents/ui/views/agent-id-view.tsx
+++ src/modules/agents/ui/views/agent-id-view.tsx
@@ -10,6 +10,7 @@ import { Badge } from "@/components/ui/badge";
 import { VideoIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
+import { useConfirm } from "../../hooks/use-confirm";
 
 interface Props {
     agentId: string;
@@ -39,41 +40,56 @@ export const AgentIdView = ({ agentId }: Props ) => {
         }),
     );
 
+    const [RemoveConfirmation , confirmRemove] = useConfirm(
+        "Are you sure you want to remove this agent?",
+        `The following action will remove ${data.meetingCount} associated meetings .`,
+    )
+
+    const handleRemoveAgent = async () => {
+        const ok = await confirmRemove();
+        if(!ok) return;
+
+        await removeAgent.mutate({id: agentId});
+    }
+
     return(
-        <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4 ">
-            <AgentIdViewHeader
-                agentId = {agentId}
-                agentName = {data.name}
-                onEdit = {() => {}}
-                onRemove = {() => removeAgent.mutate({id: agentId})}
-             />
-            
-            <div className="bg-white rounded-lg border">
-                <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
-                    <div className="flex items-center gap-x-3">
-                        <GeneratedAvatar
-                            seed={data.name}
-                            variant="botttsNeutral"
-                            className="size-10"
-                        />
+        <>
+            <RemoveConfirmation/>
+            <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4 ">
+                <AgentIdViewHeader
+                    agentId = {agentId}
+                    agentName = {data.name}
+                    onEdit = {() => {}}
+                    onRemove = {handleRemoveAgent}
+                />
+                
+                <div className="bg-white rounded-lg border">
+                    <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
+                        <div className="flex items-center gap-x-3">
+                            <GeneratedAvatar
+                                seed={data.name}
+                                variant="botttsNeutral"
+                                className="size-10"
+                            />
 
-                        <h2 className="text-2xl font-medium">{data.name}</h2>
-                    </div>
-                    <Badge 
-                        variant="outline"
-                        className="flex items-center gap-x-2 [&>svg]:size-4"
-                    >
-                        <VideoIcon className="text-blue-700"/>
-                        {data.meetingCount} {data.meetingCount === 1 ? "Meeting" : "Meetings"}
-                    </Badge>
+                            <h2 className="text-2xl font-medium">{data.name}</h2>
+                        </div>
+                        <Badge 
+                            variant="outline"
+                            className="flex items-center gap-x-2 [&>svg]:size-4"
+                        >
+                            <VideoIcon className="text-blue-700"/>
+                            {data.meetingCount} {data.meetingCount === 1 ? "Meeting" : "Meetings"}
+                        </Badge>
 
-                    <div className="flex flex-col gap-y-4">
-                        <p className="text-lg font-medium">Instructions</p>
-                        <p className="text-neutral-800">{data.instructions}</p>
+                        <div className="flex flex-col gap-y-4">
+                            <p className="text-lg font-medium">Instructions</p>
+                            <p className="text-neutral-800">{data.instructions}</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
