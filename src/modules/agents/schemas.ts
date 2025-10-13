2a8a9fa3852bb9a2e68f33e4fc50fd77886f643d
Lakshay Bansal <lakshay012003@gmail.com>
2025-10-13 21:45:36 +0530

    Create agents.update procedure

--- src/modules/agents/schemas.ts
+++ src/modules/agents/schemas.ts
@@ -4,3 +4,9 @@ export const agentsInsertSchema = z.object({
     name: z.string().min(1,{message : "Name is required"}).max(200,{message : "Name is too long"}),
     instructions : z.string().min(1,{message : "Instructions are required"}).max(800,{message : "Instrcutions are too long"}),
 })
+
+
+export const agentsUpdateSchema = z.object({
+    id: z.string().min(1,{message : "Id is required"})
+});
+
